0382b72fc3670329b8cabcaf30f53075e5839dd6
Samuel Damasceno <ssamuelsales27@gmail.com>
2026-09-08 06:50:06 -0300

    chore: add android chrome favicon and update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,10 @@ export const metadata: Metadata = {
         url: '/icon-dark-32x32.png',
         media: '(prefers-color-scheme: dark)',
       },
+          {
+            url: '/android-chrome-192x192.png',
+            sizes: '192x192',
+          },
       {
         url: '/icon.svg',
         type: 'image/svg+xml',
